92f1700b51bd3e67c2b499aeed12d0afce693305
Renan Barbosa <renan.barbosa@virtus.ufcg.edu.br>
2018-04-11 12:46:48 -0300

    learning props

---
+++ learning-props/src/Title.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React from 'react'
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {
+//       name: "Unknown",
+//       lastName: "No last name"
+//     }
+//   },
+//   render: function () {
+//     return <h1>Olá {this.props.name}  {this.props.lastName}!</h1>
+//   }
+// })
+
+export default Title
---
+++ learning-props/src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+import Title from './Title'
+
+const App = React.createClass({
+  render: function () {
+    return <div><Title name='Renan' lastName='Willamy' /></div>
+  }
+})
+
+export default App
